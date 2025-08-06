a40b674d298959c371ecc72987945300c65625b9
adp-leir <arnau.dejuan1@gmail.com>
2025-08-06 12:02:51 +0000

    arnau

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 
-import { Footer } from "../components/Footer"
+import Footer from "../components/Footer"
 import { NavbarPrivate } from "../components/NavbarPrivate";
 import { NavbarPublic } from "../components/NavbarPublic";
 import { useLocation } from "react-router-dom";
